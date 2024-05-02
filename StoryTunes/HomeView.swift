4af10df96aadbb8168f1d68db7e63b497fd2ebd3
Daniel Truong <aduy1122@gmail.com>
2024-05-02 03:22:20 -0700

    home view wip

--- StoryTunes/HomeView.swift
+++ StoryTunes/HomeView.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var searchText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ZStack {
+                ExtendedView()
+                VStack{
+                    InputView(searchText:$searchText)
+                    Spacer()
+                }
+            }
+        }.navigationBarBackButtonHidden(true)
     }
 }
 
+struct ExtendedView: View {
+    var body: some View {
+        BackgroundView(bgColor:"mainGrey", topRightCircles:["mainGreen","mainGreen"], topLeftCircle:"gradientPurple")
+               VStack {
+                   HStack {
+                       Image("storytunesLogo")
+                           .resizable()
+                           .frame(width: 64, height: 64)
+                           .alignmentGuide(.top) { _ in 0 }
+                           .alignmentGuide(.leading) { _ in 9 }.padding(.leading,24)
+                       Spacer()
+                   }
+                   Spacer()
+               }
+    }
+}
+
+struct InputView: View {
+    @Binding var searchText: String
+    var body: some View {
+        TextField("", text: $searchText)
+            .placeholder(when: searchText.isEmpty) {
+                Text("Search for songs")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+            }
+            .frame(height: 42)
+            .padding(.leading, 8)
+            .background(Color.gray)
+            .foregroundColor(Color.white)
+            .cornerRadius(8)
+            .padding(.top, 120)
+            .padding(.leading, 48)
+            .padding(.trailing, 48)
+            .overlay(
+                HStack {
+                    Spacer()
+                    Image(systemName: "magnifyingglass")              .foregroundColor(.white)        .padding(.top, 120)
+                        .padding(.trailing, 60)
+                })
+    }
+}
+
+    
 #Preview {
     HomeView()
 }
--- StoryTunes/StoryTunesApp.swift
+++ StoryTunes/StoryTunesApp.swift
@@ -15,3 +15,16 @@ struct StoryTunesApp: App {
         }
     }
 }
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+}
